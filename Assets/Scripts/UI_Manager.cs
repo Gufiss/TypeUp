a3d277153a8ec2eba3eff3336c18dd146ecae615
Gufis <guferisyt@gmail.com>
2025-03-08 21:16:24 +0200

    everything and some bugs

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
+    bool music_toggle = true;
+    bool sfx_toggle = true;
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    public Sprite sfx_off;
+    public Sprite sfx_on;
+    public Sprite music_off;
+    public Sprite music_on;
+
     public void ChangeScene(int sceneIndex = 0)
     {
         if (sceneIndex >= 0 && sceneIndex < SceneManager.sceneCountInBuildSettings)
@@ -20,5 +32,39 @@ public class UI_Manager : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Application.Quit()");
+    }
+
+    public void MuteMusic(Button obj)
+    {
+        music_toggle = !music_toggle;
+        musicSource.enabled = music_toggle;
+        if(music_toggle)
+        {
+            obj.image.sprite = music_on;
+        }
+        else
+        {
+            obj.image.sprite = music_off;
+        }
+    }
+
+    public void MuteSfx(Button obj)
+    {
+        sfx_toggle = !sfx_toggle;
+        sfxSource.enabled = sfx_toggle;
+        if (sfx_toggle)
+        {
+            obj.image.sprite = sfx_on;
+        }
+        else
+        {
+            obj.image.sprite = sfx_off;
+        }
+    }
+
+    public void PlayEffect(AudioClip audioclip)
+    {
+        sfxSource.clip = audioclip;
+        sfxSource.Play();
     }
 }
